1816924d97ad685812eeb9bdcfedf7b5654d415a
mk812y <mk812y@gmail.com>
2023-08-15 21:50:59 +0200

    eventView +

---
+++ EaseMemos/EventView.swift
@@ -0,0 +1,18 @@
+//
+//  EventView.swift
+//  EaseMemos
+//
+//  Created by Михаил Куприянов on 15.8.23..
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventView()
+}
