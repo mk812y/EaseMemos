ff4be59159833ea0113c09f32a03f53a732a7236
mk812y <mk812y@gmail.com>
2023-08-15 20:31:25 +0200

    Initial Commit

---
+++ EaseMemos/EaseMemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaseMemosApp.swift
+//  EaseMemos
+//
+//  Created by Михаил Куприянов on 15.8.23..
+//
+
+import SwiftUI
+
+@main
+struct EaseMemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
