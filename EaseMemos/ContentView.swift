5083f0ce4c9ff27b527a5f24236d5b8b221c7a77
mk812y <mk812y@gmail.com>
2023-10-05 20:01:39 +0200

    func samples

--- EaseMemos/ContentView.swift
+++ EaseMemos/ContentView.swift
@@ -9,22 +9,41 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var tips: [Tip]
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(tips) { tip in
-                    Text(tip.name)
-                }
+        NavigationStack {
+            List(tips) { tip in
+                Text(tip.name)
             }
-            Text("oops")
+            .navigationTitle("Tips")
+            .toolbar {
+                Button("Samples", action: samples)
+            }
         }
+    }
+    
+    func samples() {
+        let pipa = Tip(name: "Наполнитель для котов")
+        let pupa = Tip(name: "Аренда")
+        let popa = Tip(name: "Мегафон")
+        
+        modelContext.insert(pipa)
+        modelContext.insert(pupa)
+        modelContext.insert(popa)
     }
 }
 
 #Preview {
-    ContentView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Tip.self, configurations: config)
+    for i in 1...3 {
+        let tip = Tip(name: "Example Tip \(i)")
+        container.mainContext.insert(tip)
+    }
+    return ContentView()
+        .modelContainer(container)
 }
 
 //https://www.hackingwithswift.com/quick-start/swiftdata/how-to-use-swiftdata-in-swiftui-previews
